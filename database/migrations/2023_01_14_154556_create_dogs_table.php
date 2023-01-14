95fdc7e88a6301d6541eacf60a5f83d394cb5c03
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-14 17:11:39 +0100

    add to db migration and faker seeder

--- database/migrations/2023_01_14_154556_create_dogs_table.php
+++ database/migrations/2023_01_14_154556_create_dogs_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('dogs', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('color')->nullable();
+            $table->string('slug');
             $table->timestamps();
         });
     }
--- database/seeders/DogSeeder.php
+++ database/seeders/DogSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Dog;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class DogSeeder extends Seeder
 {
@@ -12,8 +15,14 @@ class DogSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $dog = new Dog();
+            $dog->name = $faker->name();
+            $dog->color = $faker->colorName();
+            $dog->slug = Str::slug($dog->name, '-');
+            $dog->save();
+        }
     }
 }
